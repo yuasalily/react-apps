05aafe64f334b96460cfc5f7a4973e3c096eac31
yuasalily <yuasalily1011@gmail.com>
2022-07-13 14:46:23 +0000

    made mock

--- react-apps/src/App.tsx
+++ react-apps/src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
+
+
+
+function Operand(props) {
+    return (
+        <input type="text" value={props.operand}/>
+    )
+}
+
+function Operator(props) {
+    return (
+        <div>{props.operator}</div>
+    )
+}
+
+function Equation() {
+    const [ operand1, setOperand1] = useState(0);
+    const [ operator1, setOperator1] = useState("+");
+    const [ operand2, setOperand2] = useState(0);
+    const [ operator2, setOperator2] = useState("+");
+    const [ operand3, setOperand3] = useState(0);
+    const [ operator3, setOperator3] = useState("+");
+    const [ operand4, setOperand4] = useState(0);
+    const eq = "=";
+    const rightValue = 10;
+    return (
+        <div>
+            <Operand operand={operand1}/>
+            <Operator operator={operator1}/>
+            <Operand operand={operand2}/>
+            <Operator operator={operator2}/>
+            <Operand operand={operand3}/>
+            <Operator operator={operator3}/>
+            <Operand operand={operand4}/>
+            <Operator operator={eq}/>
+            <Operand operand={rightValue}/>
+        </div>
+    );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Equation/>
     </div>
   );
 }
